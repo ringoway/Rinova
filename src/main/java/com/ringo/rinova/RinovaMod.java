2f1a9d44f7fbd6f8b5e3b8166928bea9b894a271
Azat <azat.takhautdinov@gmail.com>
2025-02-10 23:54:14 +0500

    Лекция 3: Создание предмета

--- src/main/java/com/ringo/rinova/RinovaMod.java
+++ src/main/java/com/ringo/rinova/RinovaMod.java
@@ -1,7 +1,10 @@
 package com.ringo.rinova;
 
+import com.ringo.rinova.core.registry.RItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,8 +21,18 @@ public class RinovaMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
+
+        //Регистрация предметов
+        RItems.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(RItems.PINKYLITE_CRYSTAL.get());
+        }
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
