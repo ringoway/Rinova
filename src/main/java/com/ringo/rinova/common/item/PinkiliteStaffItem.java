33e143b526390906a4407c3cdfa724a91c15259a
Azat <azat.takhautdinov@gmail.com>
2025-05-23 01:09:01 +0500

    Лекция 16: Кастомные Tooltip (подсказки) для предмета

--- src/main/java/com/ringo/rinova/common/item/PinkiliteStaffItem.java
+++ src/main/java/com/ringo/rinova/common/item/PinkiliteStaffItem.java
@@ -1,23 +1,58 @@
 package com.ringo.rinova.common.item;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class PinkiliteStaffItem extends Item {
+
     public PinkiliteStaffItem(Properties properties) {
         super(properties
                 .stacksTo(1)
                 .durability(100)
                 .rarity(Rarity.UNCOMMON));
+    }
+
+    @OnlyIn(Dist.CLIENT) // Работает только на клиенте!
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+        super.appendHoverText(stack, level, tooltip, flag);
+
+        // Просто текст
+        tooltip.add(Component.translatable("item.rinova.pinkylite_stuff.tooltip") // Ключ локализации
+                .withStyle(color -> color.withColor(13806279))
+                .withStyle(ChatFormatting.ITALIC));
+
+        // Просто пробел в тексте
+        tooltip.add(Component.empty());
+
+        // Можно вывести эффект, локализация учитывается
+        String effectName = MobEffects.REGENERATION.getDescriptionId(); //получим айди эффекта
+        tooltip.add(Component.translatable(effectName) //используем ключ эффекта в нашем списке Tooltip
+                .withStyle(ChatFormatting.LIGHT_PURPLE));
+
+        // Покажет оставшееся "заряды"
+        int max = stack.getMaxDamage(); // Максимальная прочность
+        int currentDamage = stack.getDamageValue(); // Текущая прочность
+        int remaining = max - currentDamage; // Оставшаяся прочность
+        tooltip.add(Component.translatable("item.rinova.pinkylite_stuff.tooltip.charges", remaining)
+                .withStyle(ChatFormatting.GRAY)
+                .withStyle(ChatFormatting.UNDERLINE));
     }
 
     @Override
