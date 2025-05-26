8a739d68b6239079b6d4bd22a92a016502bab3f8
Azat <azat.takhautdinov@gmail.com>
2025-05-26 20:14:32 +0500

    Лекция 20: Основы Data Generators. Теория и запуск через GatherDataEvent

---
+++ src/main/java/com/ringo/rinova/core/datagen/RDataGenerator.java
@@ -0,0 +1,40 @@
+package com.ringo.rinova.core.datagen;
+
+import com.ringo.rinova.RinovaMod;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = RinovaMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class RDataGenerator {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        // Генерация клиентских данных
+        if (event.includeClient()) {
+            addClientProviders(generator, packOutput, existingFileHelper);
+        }
+        // Генерация серверных данных
+        if (event.includeServer()) {
+            addServerProviders(generator, packOutput, lookupProvider, existingFileHelper);
+        }
+    }
+    private static void addClientProviders(DataGenerator generator, PackOutput packOutput,
+                                           ExistingFileHelper existingFileHelper) {
+        // Тут клиентские генераторы данных
+    }
+    private static void addServerProviders(DataGenerator generator, PackOutput packOutput,
+                                           CompletableFuture<HolderLookup.Provider> lookupProvider, ExistingFileHelper existingFileHelper) {
+        // Тут серверные генераторы данных
+    }
+}
