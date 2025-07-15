1c33b86ed49fda39ec0ed73cc2f8ca4e155dbac3
Azat <azat.takhautdinov@gmail.com>
2025-07-15 21:49:07 +0500

    Лекция 24: DataGen: Лут-таблицы для блоков (Loot Tables)

--- src/main/java/com/ringo/rinova/core/datagen/RDataGenerator.java
+++ src/main/java/com/ringo/rinova/core/datagen/RDataGenerator.java
@@ -1,6 +1,7 @@
 package com.ringo.rinova.core.datagen;
 
 import com.ringo.rinova.RinovaMod;
+import com.ringo.rinova.core.datagen.providers.loot.RBlockLootProvider;
 import com.ringo.rinova.core.datagen.providers.models.RBlockStateProvider;
 import com.ringo.rinova.core.datagen.providers.models.RItemModelProvider;
 import com.ringo.rinova.core.datagen.providers.recipe.RRecipeProvider;
@@ -9,11 +10,15 @@ import com.ringo.rinova.core.datagen.providers.tags.RItemTagsProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 @Mod.EventBusSubscriber(modid = RinovaMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -48,6 +53,10 @@ public class RDataGenerator {
                                            CompletableFuture<HolderLookup.Provider> lookupProvider, ExistingFileHelper existingFileHelper) {
         // Генерация рецептов
         generator.addProvider(true, new RRecipeProvider(packOutput));
+
+        // Генерация лут-таблиц
+        generator.addProvider(true, new LootTableProvider(packOutput, Set.of(), List.of(
+                new LootTableProvider.SubProviderEntry(RBlockLootProvider::new, LootContextParamSets.BLOCK))));
 
         // Генерация тегов блоков и предметов
         RBlockTagsProvider blockTagGenerator = generator.addProvider(true,
